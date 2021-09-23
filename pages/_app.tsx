05f445a764de619b3390213be8ed6cc7204baf01
fmsilva1996 <fmsilva1996@gmail.com>
2021-09-23 19:10:00 +0000

    Config styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import Theme from 'src/theme'
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Jost', sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default MyApp
